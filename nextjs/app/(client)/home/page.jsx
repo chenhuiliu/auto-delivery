cd945ac3cca1ef5cff74fc0a7259d25c065b5fbb
lifenglu <lifenglu@wisers.com>
2024-09-29 16:11:55 +0800

    aa

--- nextjs/app/(client)/home/page.jsx
+++ nextjs/app/(client)/home/page.jsx
@@ -1,7 +1,15 @@
-import { Feed } from '@/page-components/Feed';
+'use client'
 import Head from 'next/head';
-
+import { useRouter } from 'next/navigation';
+import { useCurrentUser } from '@/lib/user';
 const FeedPage = () => {
+  const router = useRouter();
+  const { data: { user } = {}, mutate } = useCurrentUser();
+  if (!user) {
+    router.push('/login');
+    return null;
+  }
+
   return (
     <>
       <Head>
@@ -13,5 +21,4 @@ const FeedPage = () => {
     </>
   );
 };
-
 export default FeedPage;
